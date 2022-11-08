4f36157d3707d6d6cbfbcc4a5014a31b0c098d54
jonas <jonas.alfredsson@protonmail.com>
2022-11-08 17:50:53 +0100

    Do not major tag development releases
    All stable releases have an even minor number, and all development
    releases have an odd number. I think we want to build all releases,
    but in order to not mess with those using the major tag here we will
    have our major tag only follow the stable releases.

--- build_release.sh
+++ build_release.sh
@@ -21,7 +21,7 @@ build () {
         --platform linux/amd64,linux/386,linux/arm64,linux/arm/v7 \
         --build-arg KEA_VERSION=${KEA_VERSION} \
         --target "${1}-target" \
-        -t "jonasal/kea-${1}:$(echo ${KEA_VERSION} | cut -d. -f 1   )${2}" \
+        $(if [ $(( $(echo ${KEA_VERSION} | cut -d. -f 2 )%2 )) -eq 0 ]; then echo "-t jonasal/kea-${1}:$(echo ${KEA_VERSION} | cut -d. -f 1 )${2}"; fi) \
         -t "jonasal/kea-${1}:$(echo ${KEA_VERSION} | cut -d. -f 1-2 )${2}" \
         -t "jonasal/kea-${1}:$(echo ${KEA_VERSION} | cut -d. -f 1-3 )${2}" \
         ${3} \
