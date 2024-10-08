9b0f8653910e946d425fdf28daf09d2d27cfecec
JonasAlfredsson <26838310+JonasAlfredsson@users.noreply.github.com>
2024-10-08 11:28:45 +0000

    Kea version 2.7.3 (development)

--- .github/update_checker.sh
+++ .github/update_checker.sh
@@ -11,7 +11,7 @@ set -eo pipefail
 
 latestStable=("2" "6" "1")
 stableChanged="false"
-latestDev=("2" "7" "2")
+latestDev=("2" "7" "3")
 devChanged="false"
 
 # Query the FTP repository and iterate over each line of the content returned.
